add1dd749d906e9da9cdfeacad09bc69af642a54
JonnerPaz <jonnerpazp@gmail.com>
2026-05-31 16:39:50 -0400

    feat: Add MusicToggle Component

---
+++ proyectos/memoria/js/components/organism/MusicToggle.js
@@ -0,0 +1,43 @@
+export class MusicToggle extends HTMLElement {
+  constructor() {
+    super();
+    this.isPlaying = false;
+    // Creamos el elemento audio internamente
+    this.audio = new Audio("../../../assets/pokemon-center-bgmusic.m4a");
+    this.audio.loop = true; // Para que la música no se corte
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <button id="music-btn" class="music-control">
+        <span id="status-icon">🔇</span>
+      </button>
+    `;
+
+    this.querySelector("#music-btn").addEventListener("click", () =>
+      this.toggleMusic()
+    );
+  }
+
+  toggleMusic() {
+    const icon = this.querySelector("#status-icon");
+
+    if (this.isPlaying) {
+      this.audio.pause();
+      icon.textContent = "🔇";
+    } else {
+      this.audio
+        .play()
+        .catch((e) => console.log("El navegador requiere interacción previa"));
+      icon.textContent = "🔊";
+    }
+
+    this.isPlaying = !this.isPlaying;
+  }
+}
+
+customElements.define("music-toggle", MusicToggle);
