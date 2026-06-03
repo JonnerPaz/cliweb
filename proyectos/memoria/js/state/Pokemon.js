91525e76821cc79f98d7bf724135d18b46454c18
JonnerPaz <jonnerpazp@gmail.com>
2026-06-02 22:49:52 -0400

    feat: Adjust Pokemon Entity to match API responses

--- proyectos/memoria/js/state/Pokemon.js
+++ proyectos/memoria/js/state/Pokemon.js
@@ -1,5 +1,40 @@
-export class Pokemons {
-  constructor(pokemons) {
-    this.pokemons = pokemons;
+export class PokemonEntity {
+  #id = 0;
+  #name = "";
+  #cries = { latest: "", legacy: "" };
+  #types = [{ type: { name: "" } }];
+  #sprites = { front: "", back: "" };
+
+  /**
+   * @param {string} name - Nombre del pokemon
+   * @param {import("./Pokemon.dto.js").Cries} cries - Gritos del pokemon
+   * @param {import("./Pokemon.dto.js").Type[]} types - Tipos del pokemon
+   * @param {import("./Pokemon.dto.js").Sprites} sprites - Sprites del pokemon
+   */
+  constructor({ name, cries, types, sprites }) {
+    this.#name = name ?? "";
+    this.#cries = cries ?? { latest: "", legacy: "" };
+    this.#types = types ?? [];
+    this.#sprites = sprites ?? { front: "", back: "" };
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get types() {
+    return this.#types;
+  }
+
+  get sprites() {
+    return this.#sprites;
+  }
+
+  get cries() {
+    return this.#cries;
   }
 }
