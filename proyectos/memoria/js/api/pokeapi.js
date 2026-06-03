6b876a391026ba68b7f749303e927019ed324415
JonnerPaz <jonnerpazp@gmail.com>
2026-06-02 23:17:51 -0400

    feat: Add Result pattern to pokemon api
    La idea de este patrón es que se pueda devolver un resultado de una operación
    exitosa o un error, en lugar de utilizar try-catch cada vez que queramos
    consultar a la api en las clases/funciones que requieran de la api.

--- proyectos/memoria/js/api/pokeapi.js
+++ proyectos/memoria/js/api/pokeapi.js
@@ -1,4 +1,5 @@
 import { PokemonEntity } from "../state/Pokemon.js";
+import { Result } from "../utils/Result.js";
 
 export class PokeApi {
   // Cantidad máxima de pokemons que existen (hasta la 9na generación)
@@ -10,28 +11,47 @@ export class PokeApi {
 
   /**
    * @param {number} id - Id del pokemon
-   * @returns {Promise<PokemonEntity>} - Objeto con los datos del pokemon
+   * @returns {Promise<Result<PokemonEntity, Error>>} - Resultado con los datos del pokemon
    */
   async getPokemonById(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    /** @type {import("./Pokemon.dto.js").PokemonResponse} */
-    const pokemonRes = await response.json();
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      if (!response.ok) {
+        return Result.err(new Error(`Error HTTP: ${response.status}`));
+      }
+      /** @type {import("./Pokemon.dto.js").PokemonResponse} */
+      const pokemonRes = await response.json();
 
-    return new PokemonEntity(pokemonRes.name);
+      return Result.ok(new PokemonEntity(pokemonRes.name));
+    } catch (error) {
+      return Result.err(error);
+    }
   }
 
   /**
    * @param {string} name - Nombre del pokemon
-   * @returns {Promise<PokemonEntity>} - Objeto con los datos del pokemon
+   * @returns {Promise<Result<PokemonEntity, Error>>} - Resultado con los datos del pokemon
    */
   async getPokemonByName(name) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    /** @type {import("./Pokemon.dto.js").PokemonResponse} */
-    const pokemonRes = await response.json();
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      if (!response.ok) {
+        return Result.err(new Error(`Error HTTP: ${response.status}`));
+      }
 
-    return new PokemonEntity(pokemonRes.name);
+      /** @type {import("./Pokemon.dto.js").PokemonResponse} */
+      const pokemonRes = await response.json();
+
+      return Result.ok(new PokemonEntity(pokemonRes.name));
+    } catch (error) {
+      return Result.err(error);
+    }
   }
 
+  /**
+   * @param {number} max
+   * @returns {Promise<Result<PokemonEntity, Error>>}
+   */
   async getRandomPokemon(max) {
     if (max < 1 || max > PokeApi.MAX_POKEMON_NUM) {
       return this.getRandomPokemon(PokeApi.MAX_POKEMON_NUM);
