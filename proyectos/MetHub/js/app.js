10462781466d7e451afcf54d58050fdcf5d8319a
Vanessa Perez <vaneppv@gmail.com>
2026-06-27 21:14:01 -0400

    Deleted test code for card in app.js

--- proyectos/MetHub/js/app.js
+++ proyectos/MetHub/js/app.js
@@ -1,23 +1,5 @@
 ﻿import { Card } from './components/card.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Prueba del carrds 
-  const app = document.getElementById('app');
-
-  if (!app) {
-    console.error('No se encontró el contenedor #app');
-    return;
-  }
-
-  const miTarjeta = document.createElement('met-card');
-
-  miTarjeta.data = {
-    title: 'Prueba',
-    artistDisplayName: 'Artista',
-    primaryImageSmall: 'https://images.metmuseum.org/CRDImages/ep/original/DP-14201-001.jpg'
-  };
-
-  app.appendChild(miTarjeta);
-
-  console.log('App inyectada y met-card renderizada');
+ 
 });
